cbd985b5dfb4e8c2f69c979b2f845dd87d75bd3e
Matthew Heritage <destroyer273@hotmail.co.uk>
2026-01-22 20:24:56 +0000

    Refactor in to object literals

--- source/content.js
+++ source/content.js
@@ -1,24 +1,154 @@
 
 
+const Directions = {
+    UP: {
+        actionName: "Close Tab",
+        calculateDirection: ({ x, y }) => {
+            return (y > gestureActivateThreshold
+                && Math.abs(x) < gestureOtherAxisAllowance);
+        },
+        activate: () => {
+            console.log("Matto - Close Tab");
+            chrome.runtime.sendMessage({ action: "CLOSE_TAB" });
+            return true;
+        },
+
+    },
+
+    DOWN: {
+        actionName: "Refresh Tab",
+        calculateDirection: ({ x, y }) => {
+            return (y < -gestureActivateThreshold
+                && Math.abs(x) < gestureOtherAxisAllowance)
+        },
+
+        activate: () => {
+            console.log("Matto - Reload Page");
+            location.reload()
+            return true;
+        },
+    },
+
+    RIGHT: {
+        actionName: "Next Tab",
+        calculateDirection: ({ x, y }) => {
+            return (x < -gestureActivateThreshold
+                && Math.abs(y) < gestureOtherAxisAllowance);
+        },
+        activate: () => {
+            console.log("Matto - Next Tab");
+            chrome.runtime.sendMessage({ action: "NEXT_TAB" });
+            return true;
+        },
+
+    },
+
+    LEFT: {
+        actionName: "Previous Tab",
+        calculateDirection: ({ x, y }) => {
+            return (x > gestureActivateThreshold
+                && Math.abs(y) < gestureOtherAxisAllowance);
+        },
+        activate: () => {
+            console.log("Matto - Previous Tab");
+            chrome.runtime.sendMessage({ action: "PREVIOUS_TAB" });
+            return true;
+        },
+
+    },
+
+    CANCEL: {
+        actionName: "Cancel",
+        calculateDirection: ({ x, y }) => {
+            const vectorLength = Math.abs(y) + Math.abs(x);
+            return vectorLength > 20 || isGesturing;
+        },
+        activate: () => {
+            console.log("Matto - Cancel");
+            return true;
+        },
+
+    },
+}
+
+let createdWidget;
+const overlayWidget = {
+    createdWidget: null,
+    createWidget: () => {
+        const { x, y } = startMousePos;
+
+        //TODO Fix this magic number
+        const posY = y - 40;
+        const posX = x - (widgetWidth / 2);
+
+        //Only allowing one instance of the overlay widget
+        if (createdWidget) {
+
+            createdWidget.style.top = posY + "px";
+            createdWidget.style.left = posX + "px";
+            overlayWidget.setShowOverlay(false);
+            return;
+        }
+
+        createdWidget = document.createElement('div');
+
+
+
+        createdWidget.style.position = 'fixed';
+        createdWidget.style.top = posY + "px";
+        createdWidget.style.left = posX + "px";
+
+        createdWidget.style.zIndex = '5000';
+        createdWidget.style.width = widgetWidth + 'px';
+        createdWidget.style.height = 'auto';
+        createdWidget.style.backgroundColor = 'black';
+        createdWidget.style.color = 'white';
+        createdWidget.style.padding = '8px';
+        createdWidget.style.textAlign = 'center';
+        createdWidget.style.fontFamily = "Arial, sans-serif";
+        createdWidget.style.fontSize = "15px";
+
+        createdWidget.textContent = 'None';
+
+        overlayWidget.setShowOverlay(false);
+
+        document.documentElement.appendChild(createdWidget);
+    },
+    setWidgetText: (inText) => {
+        if (createdWidget) {
+            createdWidget.textContent = inText;
+        }
+    },
+    setShowOverlay: (shouldShow) => {
+        if (createdWidget) {
+            createdWidget.style.display = shouldShow ? 'block' : 'none';
+        }
+    }
+}
+
+
 console.log("Registered Matto Gestures");
 
+
+//TODO Clean these vars up
 let startMousePos;
 
 let gestureActivateThreshold = 100;
 let gestureOtherAxisAllowance = 20;
 
-let isTryingGesture = false;
-
-const Direction = Object.freeze({
-    UP: 'UP',
-    DOWN: 'DOWN',
-    LEFT: 'LEFT',
-    RIGHT: 'RIGHT',
-    NONE: 'NONE'
-});
+let isHoldingMouseTwo = false;
 
 let widgetOverlay;
+const widgetWidth = 150;
 
+let isThrottled = false;
+
+let isGesturing = false;
+
+// ~
+
+
+//Get user settings from storage
 chrome.storage.sync.get(['config'], (result) => {
     if (result.config) {
         gestureActivateThreshold = result.config.activateValue;
@@ -32,181 +162,66 @@ chrome.storage.sync.get(['config'], (result) => {
 //Start mouse gesture
 document.addEventListener('mousedown', (event) => {
     if (event.button === 2) {
-        isTryingGesture = true;
+
+        isGesturing = false;
+        isHoldingMouseTwo = true;
         startMousePos = { x: event.x, y: event.y };
-        CreateWidget(startMousePos, "Hello");
+        overlayWidget.createWidget();
     }
 });
 
-let isThrottled = false;
 
 //Start mouse gesture
 document.addEventListener('mousemove', (event) => {
 
+    if (!isThrottled && isHoldingMouseTwo) {
 
-    if (isThrottled || !isTryingGesture) return;
+        isThrottled = true;
 
-    //isThrottled = true;
+        let strText = "None";
 
-    const dir = calculateGesture({ x: event.x, y: event.y });
-    console.log(dir);
+        const normalisedMouseX = startMousePos.x - event.x;
+        const normalisedMouseY = startMousePos.y - event.y;
 
-    let strText = "None";
-    switch (dir) {
-        case Direction.UP: {
-            strText = "Close Tab";
-            break;
+        const activeLogic = Object.values(Directions).find(item =>
+            item.calculateDirection({ x: normalisedMouseX, y: normalisedMouseY }));
+
+        if (activeLogic) {
+            strText = activeLogic.actionName;
+
+            if (activeLogic === Directions.CANCEL) {
+                if (!isGesturing)
+                    overlayWidget.setShowOverlay(true);
+
+                isGesturing = true;
+            }
         }
-        case Direction.DOWN: {
-            strText = "Refresh Tab";
-            break;
-        }
-        case Direction.RIGHT: {
-            strText = "Next Tab";
-            break;
-        }
-        case Direction.LEFT: {
-            strText = "Previous Tab";
-            break;
-        }
+
+        overlayWidget.setWidgetText(strText);
+
+
+        setTimeout(() => {
+            isThrottled = false;
+        }, 50);
     }
-    SetWidgetText(strText);
-
-
-    setTimeout(() => {
-        isThrottled = false;
-    }, 200);
 
 });
 
 //Complete gesture
 document.addEventListener('contextmenu', (event) => {
 
-    isTryingGesture = false;
-    const endMousePos = { x: event.x, y: event.y };
-    HideWidet();
+    isHoldingMouseTwo = false;
+    overlayWidget.setShowOverlay(false);
 
-    const dir = calculateGesture(endMousePos);
-    switch (dir) {
-        case Direction.UP:
-            {
-                // Close tab
-                console.log("Matto - Close Tab");
+    const normalisedMouseX = startMousePos.x - event.x;
+    const normalisedMouseY = startMousePos.y - event.y;
 
-                event.preventDefault();
-                chrome.runtime.sendMessage({ action: "CLOSE_TAB" });
-                return;
-            }
-        case Direction.DOWN:
-            {
-                // Reload page
-                console.log("Matto - Reload Page");
+    const activeLogic = Object.values(Directions).find(direction =>
+        direction.calculateDirection({ x: normalisedMouseX, y: normalisedMouseY }));
 
-                event.preventDefault();
-                location.reload()
-                return;
-            }
-        case Direction.RIGHT:
-            {
-                // Next tab
-                console.log("Matto - Next Tab");
-
-                event.preventDefault();
-                chrome.runtime.sendMessage({ action: "NEXT_TAB" });
-                return;
-            }
-        case Direction.LEFT:
-            {
-                // Previous Tab
-                console.log("Matto - Previous Tab");
-
-                event.preventDefault();
-                chrome.runtime.sendMessage({ action: "PREVIOUS_TAB" });
-                return;
-            }
+    if (activeLogic) {
+        activeLogic.activate()
+        event.preventDefault();
     }
-
-
 }, true);
 
-
-// Widget Overlay
-
-CreateWidget = (offset, triggeredAction) => {
-
-    const { x, y } = startMousePos;
-
-    //TODO Instead of recreating this, we should just reuse the same object!
-    widgetOverlay = document.createElement('div');
-
-
-    //TODO Offset the position to be above the mouse!
-    widgetOverlay.style.position = 'fixed';
-    widgetOverlay.style.top = y + "px";
-    widgetOverlay.style.left = x + "px";
-
-    widgetOverlay.style.zIndex = '5000';
-    widgetOverlay.style.width = 'auto';
-    widgetOverlay.style.height = 'auto';
-    widgetOverlay.style.backgroundColor = 'black';
-    widgetOverlay.style.color = 'white';
-    widgetOverlay.style.padding = '8px';
-
-    widgetOverlay.textContent = 'Viewport Coordinate:' + x + ', ' + y;
-
-    document.documentElement.appendChild(widgetOverlay);
-}
-
-SetWidgetText = (inText) => {
-    if (widgetOverlay) {
-        widgetOverlay.textContent = inText;
-    }
-}
-
-HideWidet = () => {
-    if (widgetOverlay) {
-
-        const hideWidget = (widgetInstance) => {
-            widgetInstance.style.display = 'none';
-        };
-
-        setTimeout(hideWidget, 500, widgetOverlay);
-    }
-}
-
-
-// Directions
-
-const calculateGesture = (endMousePos) => {
-
-    const normalisedMouseX = startMousePos.x - endMousePos.x;
-    const normalisedMouseY = startMousePos.y - endMousePos.y;
-
-    // Reload page
-    if (normalisedMouseY < -gestureActivateThreshold
-        && Math.abs(normalisedMouseX) < gestureOtherAxisAllowance) {
-
-        return Direction.DOWN;
-    }
-
-    // Close tab
-    if (normalisedMouseY > gestureActivateThreshold
-        && Math.abs(normalisedMouseX) < gestureOtherAxisAllowance) {
-
-        return Direction.UP;
-    }
-
-    // Next tab
-    if (normalisedMouseX < -gestureActivateThreshold
-        && Math.abs(normalisedMouseY) < gestureOtherAxisAllowance) {
-        return Direction.RIGHT;
-    }
-
-    // Previous Tab
-    if (normalisedMouseX > gestureActivateThreshold
-        && Math.abs(normalisedMouseY) < gestureOtherAxisAllowance) {
-        return Direction.LEFT;
-    }
-
-    return Direction.NONE;
-}
