d574556603f147d210d9b3b48201741a8db5739b
Matthew Heritage <destroyer273@hotmail.co.uk>
2026-01-22 14:08:38 +0000

    Added close tab gesture,

--- source/background.js
+++ source/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-   
+
     if (message.action === "NEXT_TAB" || message.action === "PREVIOUS_TAB") {
 
         const increment = message.action === "NEXT_TAB" ? 1 : -1;
@@ -18,4 +18,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         });
     }
 
+
+    else if (message.action === "CLOSE_TAB") {
+        chrome.tabs.query({ currentWindow: true }, (tabs) => {
+
+            const currentTabID = sender.tab.id;
+            if (currentTabID) {
+                chrome.tabs.remove(currentTabID);
+            }
+        });
+    }
+
 });
+
--- source/content.js
+++ source/content.js
@@ -25,38 +25,46 @@ document.addEventListener('contextmenu', (event) => {
     // Reload page
     if (normalisedMouseY < -gestureActivateThreshold
         && Math.abs(normalisedMouseX) < gestureOtherAxisAllowance) {
+
         console.log("Matto - Reload Page");
 
         event.preventDefault();
-
         location.reload()
-        return;
 
+        return;
+    }
+
+    // Close tab
+    if (normalisedMouseY > gestureActivateThreshold
+        && Math.abs(normalisedMouseX) < gestureOtherAxisAllowance) {
+
+        console.log("Matto - Close Tab");
+
+        event.preventDefault();
+        chrome.runtime.sendMessage({ action: "CLOSE_TAB" });
+        return;
     }
 
     // Next tab
     if (normalisedMouseX < -gestureActivateThreshold
         && Math.abs(normalisedMouseY) < gestureOtherAxisAllowance) {
+
         console.log("Matto - Next Tab");
 
         event.preventDefault();
         chrome.runtime.sendMessage({ action: "NEXT_TAB" });
-
-
         return;
     }
 
     // Previous Tab
     if (normalisedMouseX > gestureActivateThreshold
         && Math.abs(normalisedMouseY) < gestureOtherAxisAllowance) {
+
         console.log("Matto - Previous Tab");
 
         event.preventDefault();
-
         chrome.runtime.sendMessage({ action: "PREVIOUS_TAB" });
-
         return;
-
     }
 }, true);
 
