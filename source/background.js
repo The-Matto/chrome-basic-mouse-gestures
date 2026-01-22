2e3e46488c60952c1ac6f5d433826b914617c05b
Matthew Heritage <destroyer273@hotmail.co.uk>
2026-01-22 13:39:27 +0000

    Created the basic mouse gestures

---
+++ source/background.js
@@ -0,0 +1,21 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+   
+    if (message.action === "NEXT_TAB" || message.action === "PREVIOUS_TAB") {
+
+        const increment = message.action === "NEXT_TAB" ? 1 : -1;
+
+        chrome.tabs.query({ currentWindow: true }, (tabs) => {
+
+            const currentTabIndex = sender.tab.index;
+
+            //Handle tab wrapping
+            const nextTabIndex = (currentTabIndex + increment) % tabs.length;
+
+            const nextTab = tabs.find(t => t.index === nextTabIndex);
+            if (nextTab) {
+                chrome.tabs.update(nextTab.id, { active: true });
+            }
+        });
+    }
+
+});
---
+++ source/content.js
@@ -0,0 +1,62 @@
+console.log("Registered Matto Gestures");
+
+let startMousePos;
+
+const gestureActivateThreshold = 100.;
+const gestureOtherAxisAllowance = 20.;
+
+//Start mouse gesture
+document.addEventListener('mousedown', (event) => {
+    if (event.button === 2) {
+
+        startMousePos = { x: event.x, y: event.y };
+
+    }
+});
+
+//Complete gesture
+document.addEventListener('contextmenu', (event) => {
+
+    const endMousePos = { x: event.x, y: event.y };
+
+    const normalisedMouseX = startMousePos.x - endMousePos.x;
+    const normalisedMouseY = startMousePos.y - endMousePos.y;
+
+    // Reload page
+    if (normalisedMouseY < -gestureActivateThreshold
+        && Math.abs(normalisedMouseX) < gestureOtherAxisAllowance) {
+        console.log("Matto - Reload Page");
+
+        event.preventDefault();
+
+        location.reload()
+        return;
+
+    }
+
+    // Next tab
+    if (normalisedMouseX < -gestureActivateThreshold
+        && Math.abs(normalisedMouseY) < gestureOtherAxisAllowance) {
+        console.log("Matto - Next Tab");
+
+        event.preventDefault();
+        chrome.runtime.sendMessage({ action: "NEXT_TAB" });
+
+
+        return;
+    }
+
+    // Previous Tab
+    if (normalisedMouseX > gestureActivateThreshold
+        && Math.abs(normalisedMouseY) < gestureOtherAxisAllowance) {
+        console.log("Matto - Previous Tab");
+
+        event.preventDefault();
+
+        chrome.runtime.sendMessage({ action: "PREVIOUS_TAB" });
+
+        return;
+
+    }
+}, true);
+
